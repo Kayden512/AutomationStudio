a8c12237f367bb5bff6eda38bb946ba7c9459c08
kiseong1.kim <kiseong1.kim@samsung.com>
2025-05-15 00:26:43 +0900

    PropertyGrid DarkMode

--- Automation.PluginCore/Base/Device/PLC/TestTool.cs
+++ Automation.PluginCore/Base/Device/PLC/TestTool.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Automation.PluginCore.Interface;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,10 @@ namespace Automation.PluginCore.Base.Device.PLC
 {
     public class TestTool : DeviceBase
     {
+        public double Double { get; set; }
+        public int Int { get; set; }
+
+        public ObservableCollection<INode> Collection { get; set; } = new ObservableCollection<INode>(); 
+
     }
 }
